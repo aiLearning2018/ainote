af4adee8428807a1f12001871c5dffbef45ebb39
yangfeng <yangfeng@sogou-inc.com>
2017-12-04 17:07:24 +0800

    sort items by occurs count descend and alphabat order

--- python_ex/stastic_word/count_words.py
+++ python_ex/stastic_word/count_words.py
@@ -1,4 +1,19 @@
+import collections
 
 def count_words(text, segment_num):
     # todo: implement method rather than return hard code sample
-    return [('butter', 2), ('a', 1), ('betty', 1)]
+    # return [('butter', 2), ('a', 1), ('betty', 1)]
+    # a = {}
+    sp = text.split()
+    words = sorted(sp)
+    a = collections.OrderedDict()
+    for w in words:
+        if a.has_key(w):
+            a[w] += 1
+        else:
+            a[w] = 1
+
+    sorted_items = sorted(a.iteritems(), key=lambda a:a[1], reverse=True)
+    # print sorted_items
+
+    return sorted_items[0:segment_num]
