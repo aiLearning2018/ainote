3fb4a19bdd6f72a4675e6e03f0f7c96b3c944230
yangfeng <yangfeng@sogou-inc.com>
2017-12-07 14:21:10 +0800

    mini project ex to draw flower, fractals and write initials

---
+++ python_basic/ch3_turtule_mini_project.py
@@ -0,0 +1,103 @@
+import turtle
+
+def draw_fill_triangle(t, length, degree):
+    for i in range(1, 4):
+        t.begin_fill()
+        t.fd(length)
+        t.left(degree)
+        t.fd(length)
+        t.end_fill()
+
+def move_turtle(t, length):
+    t.fd(length)
+
+def rotate_move_turtle(t, length, degree):
+    t.penup()
+    t.right(degree)
+    move_turtle(t, length)
+    t.left(degree)
+
+def draw_fractal(t, length, degree):
+    draw_fill_triangle(t, length, degree)
+
+    move_len = 2 * length
+    rotate_move_turtle(t, move_len, degree)
+
+    draw_fill_triangle(t, length, degree)
+
+    t.fd(move_len)
+    draw_fill_triangle(t, length, degree)
+
+def new_turtle(co, sp, sh):
+    t = turtle.Turtle()
+    t.shape(sh)
+    t.color(co)
+    t.speed(sp)
+    return t
+
+def draw_fractals():
+    t = new_turtle("green", "fastest", "turtle")
+
+    t.fill(True)
+    t.fillcolor("green")
+
+    draw_fractal(t, 20, 120)
+
+    rotate_move_turtle(t, 40, 120)
+    rotate_move_turtle(t, 40, 180)
+    draw_fractal(t, 20, 120)
+
+    rotate_move_turtle(t, 40, 240)
+    move_turtle(t, 80)
+    draw_fractal(t, 20, 120)
+
+
+def draw_flower():
+    t = new_turtle("blue", "fastest", "turtle")
+    rotate_move_turtle(t, 300, 180)
+
+    t.pendown()
+    for i in range(0, 90):
+        t.right(4)
+        t.fd(100)
+        t.right(60)
+        t.fd(100)
+        t.right(120)
+        t.fd(100)
+        t.right(60)
+        t.fd(100)
+        t.right(120)
+    t.right(90)
+    t.fd(300)
+    pass
+
+def write_initials():
+    t = new_turtle("blue", "fastest", "turtle")
+
+    t.right(90)
+    t.fd(200)
+    t.back(100)
+    t.left(45)
+    t.fd(140)
+    t.back(140)
+    t.left(90)
+    t.fd(140)
+
+    t.right(45)
+    rotate_move_turtle(t, 80, 0)
+    t.pendown()
+
+    t.back(60)
+    t.right(90)
+    t.fd(200)
+    t.left(90)
+    t.fd(60)
+
+    s = t.getscreen()
+    s.exitonclick()
+    pass
+
+# draw a fractals in the middle, a flower on the left, and write an initials on the right.
+draw_fractals()
+draw_flower()
+write_initials()
